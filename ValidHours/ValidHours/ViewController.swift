0f42badf40cdfbfcb7d20c8c9ee93fc4b4ddcc73
Kazuaki Hara <bizhara@gmail.com>
2016-03-17 11:58:47 +0900

    Show application name at navigation bar.

--- ValidHours/ValidHours/ViewController.swift
+++ ValidHours/ValidHours/ViewController.swift
@@ -9,17 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.title = NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleExecutable") as? String
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
